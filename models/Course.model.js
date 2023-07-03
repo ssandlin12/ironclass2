501cd708a772d193a1e85659a23f9a93f23fcdad
Mirza Abdullah Baig <abdullah.baig2013@gmail.com>
2023-07-03 17:30:06 +0200

    Sign up and login page from abdullah

---
+++ models/Course.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  instructor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  categories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  ],
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("Course", courseSchema);
